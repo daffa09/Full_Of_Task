a02a6dd5fb5843ce0a001a91e1bc3243aec2b42c
Daffa_fathan <68214221+daffa09@users.noreply.github.com>
2020-10-29 13:47:23 +0700

    Tugas Baru

---
+++ Aplikasi Sederhana (Olshop)/src/com/daftar/Pembeli.java
@@ -0,0 +1,13 @@
+package com.daftar; // ini adalah package kedua nya yang berguna sebagai repository daftar penjual dan pembeli pada
+// aplikasi ini
+
+public class Pembeli {
+    
+    public void pembeli(){
+
+        System.out.println("\n==== PEMBELI ====");
+        System.out.println("1. Dudung");
+        System.out.println("2. Bayu");
+        System.out.println("3. Saburo");
+    }
+}
---
+++ Aplikasi Sederhana (Olshop)/src/com/daftar/Penjual.java
@@ -0,0 +1,14 @@
+package com.daftar; // ini adalah package kedua nya yang berguna sebagai repository daftar penjual dan pembeli pada
+// aplikasi ini
+
+public class Penjual {
+ 
+    public void penjual(){
+
+        System.out.println("\n==== PENJUAL ====");
+        System.out.println("1. Azalea");
+        System.out.println("2. Fanthom");
+        System.out.println("3. Grey");
+
+    }
+}
