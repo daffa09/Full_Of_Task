a567b92a33da8625dc11dc9a3e7954bcd9bcd5a1
Daffa_fathan <68214221+daffa09@users.noreply.github.com>
2022-02-20 13:29:39 +0700

    crud sederhana php native

---
+++ daffa fathan/proses/hapus.php
@@ -0,0 +1,22 @@
+<?php 	
+
+require 'functions.php';
+
+$nis = $_GET["nis"];
+
+if (hapus($nis) > 0) {
+		echo "
+			<script>
+			alert('Data berhasil dihapus');
+			document.location.href='../index.php';
+			</script>
+		";
+	} else{
+		echo "
+			<script>
+			alert('Data gagal dihapus');
+			document.location.href='../index.php';
+			</script>
+		";
+	}
+ ?>
